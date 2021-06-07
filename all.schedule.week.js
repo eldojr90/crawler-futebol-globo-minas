c0cfb3daa9e5f4bf19b4d092a392b27f79875f5c
Eldo Jr <eldojr90@gmail.com>
2021-06-06 21:39:33 -0300

    melhorias gerais

--- all.schedule.week.js
+++ all.schedule.week.js
@@ -2,42 +2,66 @@ const requestPromise = require('request-promise');
 const cheerio = require('cheerio');
 
 const numberToDateString = number => {
-    if(number < 10) number = `0${number}`;
+    if (number < 10) number = `0${number}`;
     return number;
 }
-const todayIni = () => new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0);
 
-const todayEnd = () => new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59);
+const daysOfWeek = [
+    'Domingo',
+    'Segunda-Feira',
+    'Terça-Feira',
+    'Quarta-Feira',
+    'Quinta-Feira',
+    'Sexta-Feira',
+    'Sábado',
+];
 
-const timeString = date =>{
+const getTodayIni = () => new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 0, 0);
+
+const getTodayEnd = () => new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59);
+
+const getDateToString = date => {
     const day = numberToDateString(date.getDate());
     const month = numberToDateString(date.getMonth());
     const fyear = date.getFullYear();
-    const h = numberToDateString(date.getHours());
-    const m = numberToDateString(date.getMinutes());
-    
-    return `${day}/${month}/${fyear} - ${h}:${m}`};
+
+    return `${day}/${month}/${fyear}`
+};
+
+const getDateToTimeString =
+    date => `${numberToDateString(date.getHours())}:${numberToDateString(date.getMinutes())}`;
 
 const allSchedulesWeek = () => requestPromise({
     uri: 'https://redeglobo.globo.com/globominas/programacao/',
     transform: body => cheerio.load(body),
 }).then($ => {
-    let schedules = [];
-    $('.schedule-item-inner').each((i, item) => {
-        const scheduleTimestamp = $(item).find('.schedule-times').attr('data-start-time');
-        const label = $(item).find('.schedule-item-content-soccer').text().trim();
-        const date = new Date(scheduleTimestamp * 1000);
-        const scheduleTimeString = timeString(date);
+        let schedules = [];
 
-        if (label !== '') {
-            schedules.push({ date, scheduleTimeString, label });
-        }
-    });
-    return schedules;
+        $('.schedule-item-inner').each((i, item) => {
+            const unixTimestamp = $(item).find('.schedule-times').attr('data-start-time');
+
+            const eventDate = new Date(unixTimestamp * 1000);
+            const description = $(item).find('.schedule-item-content-soccer').text().trim();
+            const dateString = getDateToString(eventDate);
+            const timeString = getDateToTimeString(eventDate);
+            const dayOfWeek = daysOfWeek[eventDate.getDay()];
+
+            if (description !== '' && (eventDate > getTodayIni())) {
+                schedules.push({
+                    eventDate,
+                    description,
+                    dateString,
+                    timeString,
+                    dayOfWeek,
+                });
+            }
+        });
+        
+        return schedules;
 }).catch(err => console.log(err));
 
 const schedulesToday = () => allSchedulesWeek().then(schedules =>
-    schedules.filter(schedule => schedule.date >= todayIni() && schedule.date <= todayEnd()));
+    schedules.filter(schedule => schedule.date >= getTodayIni() && schedule.date <= getTodayEnd()));
 
 module.exports = {
     allSchedulesWeek,
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const { allSchedulesWeek, schedulesToday } = require('./all.schedule.week');
+const { 
+    allSchedulesWeek, 
+    schedulesToday, 
+} = require('./all.schedule.week');
 
+/* 
+retorna os dados das trasmissões de futebol 
+a partir da data de hoje */
 allSchedulesWeek().then(data => console.log(data));
 
+//retorna somente data de hoje 
 schedulesToday().then(data => console.log(data));
